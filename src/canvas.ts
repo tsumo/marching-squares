6f5eed432ad831669b0f2f230a0fbe5a7c3cd1a9
tsumo <tsumo@users.noreply.github.com>
2021-03-19 19:34:39 +0300

    influence sampling

--- src/canvas.ts
+++ src/canvas.ts
@@ -5,22 +5,27 @@ export class Render {
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
 
+  private readonly width = 640;
+  private readonly height = 480;
+
+  private readonly gridStep = 10;
+  private readonly gridHalfStep = 5;
+
   balls: Ball[] = [];
 
   constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
     this.canvas = canvas;
     this.ctx = ctx;
-    canvas.width = 640;
-    canvas.height = 480;
-    this.ctx.strokeStyle = "salmon";
+    canvas.width = this.width;
+    canvas.height = this.height;
 
     for (let i = 0; i < 10; ++i) {
       this.balls.push(
         new Ball(
-          canvas.width,
-          canvas.height,
-          rand(canvas.width),
-          rand(canvas.height),
+          this.width,
+          this.height,
+          rand(this.width),
+          rand(this.height),
           randRange(30, 50),
           randRange(1, 2),
           rand(Math.PI * 2)
@@ -31,10 +36,29 @@ export class Render {
     const tick = () => {
       this.clear();
       this.updateBalls();
+      this.drawGrid();
       this.drawBalls();
       requestAnimationFrame(tick);
     };
     tick();
+  }
+
+  private drawGrid() {
+    this.ctx.strokeStyle = "#263339";
+    this.ctx.fillStyle = "indianred";
+    for (let x = 0; x < this.width; x += this.gridStep) {
+      for (let y = 0; y < this.height; y += this.gridStep) {
+        const influence = this.balls.reduce((prev, curr) => {
+          const xx = (x + this.gridHalfStep - curr.x) ** 2;
+          const yy = (y + this.gridHalfStep - curr.y) ** 2;
+          return curr.radius ** 2 / (xx + yy) + prev;
+        }, 0);
+        if (influence >= 1) {
+          this.ctx.fillRect(x, y, 20, 20);
+        }
+        this.ctx.strokeRect(x, y, this.gridStep, this.gridStep);
+      }
+    }
   }
 
   private updateBalls() {
@@ -42,6 +66,7 @@ export class Render {
   }
 
   private drawBalls() {
+    this.ctx.strokeStyle = "salmon";
     this.balls.forEach((ball) => {
       this.ctx.beginPath();
       this.ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
@@ -50,6 +75,6 @@ export class Render {
   }
 
   private clear() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.clearRect(0, 0, this.width, this.height);
   }
 }
